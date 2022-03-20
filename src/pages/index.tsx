e1a54a779fbc3307e88644f28204c7ec0bba336b
Alex Paris <alexlima@gruposgapetro.com.br>
2022-03-20 18:21:30 -0300

    chore: first part of index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,9 +26,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <div className={styles.contentLogo}>
+          <img src="/images/logo.svg" alt="logo" />
+        </div>
+
+        <div className={styles.contentPosts}>
+          <a>
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.contentPostsAuthor}>
+              <div>
+                <FiCalendar size="1.25rem" />
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <FiUser size="1.25rem" />
+                Joseph Oliveira
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className={styles.contentPosts}>
+          <a>
+            <h1>Como utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.contentPostsAuthor}>
+              <div>
+                <FiCalendar size="1.25rem" />
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <FiUser size="1.25rem" />
+                Joseph Oliveira
+              </div>
+            </div>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,9 +1,13 @@
-import Prismic from '@prismicio/client';
-import { DefaultClient } from '@prismicio/client/types/client';
+import { Client, createClient } from '@prismicio/client';
 
-export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
-    req,
+export function getPrismicClient(): Client {
+  const prismic = createClient(process.env.PRISMIC_API_ENDPOINT, {
+    // routes: [
+    //   {
+    //     type: 'posts',
+    //     path: '/:uid',
+    //   },
+    // ],
   });
 
   return prismic;
