13df68b83f4a29bb1e71c0feafef958b800daffb
Alex Paris <alexlima@gruposgapetro.com.br>
2022-03-22 20:02:58 -0300

    feat: RichText type and post initial model

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import ptBR from 'date-fns/locale/pt-BR';
+import { format } from 'date-fns';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import { PrismicRichText } from '@prismicio/react';
+import { RichTextField } from '@prismicio/types';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -15,9 +21,7 @@ interface Post {
     author: string;
     content: {
       heading: string;
-      body: {
-        text: string;
-      }[];
+      body: RichTextField;
     }[];
   };
 }
@@ -26,20 +30,83 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+      <main className={commonStyles.contentContainer}>
+        <img src={post.data.banner.url} alt="banner" />
 
-//   // TODO
-// };
+        <article className={styles.post}>
+          <h1>{post.data.title}</h1>
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+          <div className={commonStyles.contentPostsAuthor}>
+            <div>
+              <FiCalendar size="1.25rem" />
+              <time>{post.first_publication_date}</time>
+            </div>
+            <div>
+              <FiUser size="1.25rem" />
+              {post.data.author}
+            </div>
+            <div>
+              <FiClock size="1.25rem" />
+              {post.data.author}
+            </div>
+          </div>
 
-//   // TODO
-// };
+          <div className={styles.postContent}>
+            {post.data.content.map(content => (
+              <>
+                <h2>{content.heading}</h2>
+                <PrismicRichText field={content.body} />
+              </>
+            ))}
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    // Set a static variable loadded before in another static page
+    paths: [
+      // { params: { slug: 'the-meaning-of-life' } }
+    ],
+    // fallback
+    //  true: load the page before getting information from getStaticProps
+    //  false: return 404 if getStaticProps was not loadded
+    //  blocking: wait server side rendering before showing page
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    first_publication_date: format(
+      new Date(response.first_publication_date),
+      'dd MMM yyyy',
+      { locale: ptBR }
+    ),
+    data: response.data,
+  };
+
+  console.log(JSON.stringify(post.data.content, null, 2));
+
+  return {
+    props: {
+      post,
+    },
+    redirect: 60 * 30, // 30 minutes
+  };
+};
